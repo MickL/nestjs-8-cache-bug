814fde3aa1735ddcbe13231a45c72a09c5fb617a
mick-brandung <mick.lawitzke@brandung.de>
2021-05-21 13:46:42 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { CacheModule, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports:     [
+    CacheModule.register(),
+  ],
+  controllers: [AppController],
+  providers:   [AppService],
+})
+export class AppModule {
+}
